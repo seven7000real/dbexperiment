9dcc4dbf99e5a1cb80eb4df6afaa92ed9c838b49
Adrian <78108584+AdrianCassar@users.noreply.github.com>
2024-11-19 00:23:37 +0000

    [XAM] Improve XamUserGetSubscriptionType

--- src/xenia/xbox.h
+++ src/xenia/xbox.h
@@ -615,6 +615,7 @@ struct X_XAMACCOUNTINFO {
   };
 
   enum AccountSubscriptionTier {
+    kSubscriptionTierNone = 0,
     kSubscriptionTierSilver = 3,
     kSubscriptionTierGold = 6,
     kSubscriptionTierFamilyGold = 9
